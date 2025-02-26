e3f53664f7bc95923f2d4a0f1d2d64ec5b56ad74
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-02-26 14:42:36 +0100

    primer commit

---
+++ src/sooper/demo/tkrun/Supermercado.java
@@ -0,0 +1,51 @@
+package sooper.demo.tkrun;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Supermercado {
+
+	protected Shell shell;
+	
+	
+	
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Supermercado window = new Supermercado();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SOOPER");
+
+	}
+
+}
